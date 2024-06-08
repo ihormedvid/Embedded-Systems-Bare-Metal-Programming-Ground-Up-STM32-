e63a1196afc8c2504157a5cb7f34770c057d7c67
ihormedvid <igormedvid89@gmail.com>
2024-06-08 22:57:28 +0300

    7 uart module

---
+++ 7_uart_modular/Debug/Src/uart.d
@@ -0,0 +1,18 @@
+Src/uart.o: ../Src/uart.c ../Inc/uart.h \
+ D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ D:/libs/chip_headers/CMSIS/Include/core_cm4.h \
+ D:/libs/chip_headers/CMSIS/Include/cmsis_version.h \
+ D:/libs/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ D:/libs/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ D:/libs/chip_headers/CMSIS/Include/mpu_armv7.h \
+ D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/uart.h:
+D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+D:/libs/chip_headers/CMSIS/Include/core_cm4.h:
+D:/libs/chip_headers/CMSIS/Include/cmsis_version.h:
+D:/libs/chip_headers/CMSIS/Include/cmsis_compiler.h:
+D:/libs/chip_headers/CMSIS/Include/cmsis_gcc.h:
+D:/libs/chip_headers/CMSIS/Include/mpu_armv7.h:
+D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
