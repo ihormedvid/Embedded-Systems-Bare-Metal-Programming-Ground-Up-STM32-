48aff96bd3abeb5b676db8f5c22f4cfa2ee59349
ihormedvid <igormedvid89@gmail.com>
2024-06-15 01:25:16 +0300

    add 13 Output compare

---
+++ 13_OutputCompare/Inc/systick.h
@@ -0,0 +1,14 @@
+/*
+ * systick.h
+ *
+ *  Created on: Jun 12, 2024
+ *      Author: Ihor
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void systickDelayMs(int delay);
+
+
+#endif /* SYSTICK_H_ */
