c3e3a146662b56f8d609b777b101e0031222e927
ihormedvid <igormedvid89@gmail.com>
2024-07-02 10:35:41 +0300

    add 22 SPI adxl345

---
+++ 22_SPI_adxl345/Debug/Src/i2c.d
@@ -0,0 +1,17 @@
+Src/i2c.o: ../Src/i2c.c \
+ D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ D:/libs/chip_headers/CMSIS/Include/core_cm4.h \
+ D:/libs/chip_headers/CMSIS/Include/cmsis_version.h \
+ D:/libs/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ D:/libs/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ D:/libs/chip_headers/CMSIS/Include/mpu_armv7.h \
+ D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+D:/libs/chip_headers/CMSIS/Include/core_cm4.h:
+D:/libs/chip_headers/CMSIS/Include/cmsis_version.h:
+D:/libs/chip_headers/CMSIS/Include/cmsis_compiler.h:
+D:/libs/chip_headers/CMSIS/Include/cmsis_gcc.h:
+D:/libs/chip_headers/CMSIS/Include/mpu_armv7.h:
+D:/libs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
